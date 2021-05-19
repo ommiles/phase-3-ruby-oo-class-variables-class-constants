d34db97bbf08a8a892e9d7fe247732bb45f95ec0
OM Miles <miles@ommiles.com>
2021-05-19 08:07:07 -0700

    commit tree 83e7f9246229cc0825e6baa3e188465fe4a71c7f

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,17 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = [].uniq
+
   def initialize(brand)
     @brand = brand
+  end
+
+  # create the writer for genre and add the logic for the class constant
+  def brand=(brand)
+    @brand = brand
+    BRANDS << brand
+    # BRANDS.uniq
   end
 
   def cobble
